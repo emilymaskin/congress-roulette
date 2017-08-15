733afd18f064ad887cf1672239395fb1343248a4
Emily Maskin <emily.maskin@gmail.com>
2017-08-14 23:47:23 -0400

    cville

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,79 @@
+/* global ga */
+
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite/no-important';
 import '../css/App.css';
-import { candidates } from '../candidates';
+import { recipients } from '../recipients';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      candidate: null,
+      recipient: null,
     };
   }
 
   spinWheel = () => {
-    const candidate = this.getRandomCandidate();
+    const recipient = this.getRandomRecipient();
 
-    if (candidate && this.state.candidate && (
-      candidate.district === this.state.candidate.district)
+    if (
+      recipient
+      && this.state.recipient
+      && recipient.name === this.state.recipient.name
     ) {
       this.spinWheel();
     }
+    ga('send', 'event', 'wheel', 'spin');
 
     this.setState({
-      candidate,
+      recipient,
     });
-  }
+  };
 
-  getRandomCandidate() {
-    const randomIndex = Math.floor(Math.random() * candidates.length);
+  onClickDonate = () => {
+    ga('send', 'event', 'donateButton', 'click');
+  };
 
-    return candidates[randomIndex];
+  getRandomRecipient() {
+    const randomIndex = Math.floor(Math.random() * recipients.length);
+
+    return recipients[randomIndex];
   }
 
   render() {
     return (
       <div className="App">
         <div className={css(styles.wrapper)}>
-          {this.state.candidate ? (
-            <div>
-              <h1>You should donate to:</h1>
-              <div>
-                <a href={this.state.candidate.url} target="_blank">
-                  {this.state.candidate.name}, {this.state.candidate.district}
-                </a>
+          {this.state.recipient
+            ? <div>
+                <h1 className={css(styles.h1)}>You should donate to:</h1>
+                <h2 className={css(styles.h2)}>
+                  {this.state.recipient.name}
+                </h2>
+                <div>
+                  <a
+                    href={this.state.recipient.url}
+                    target="_blank"
+                    className={css(styles.button)}
+                    onClick={this.onClickDonate}
+                  >
+                    Donate here
+                  </a>
+                </div>
+                <div onClick={this.spinWheel} className={css(styles.spinAgain)}>
+                  Spin again
+                </div>
               </div>
-              <div onClick={this.spinWheel} className={css(styles.button)}>
-                Spin again
-              </div>
-            </div>
-          ) : (
-            <div>
-              <div>
-                Looking to contribute to a race outside your safely blue
-                bubble? Find a Democratic candidate here.
-              </div>
-              <div onClick={this.spinWheel} className={css(styles.button)}>
-                Spin the wheel
-              </div>
-            </div>
-          ) }
+            : <div>
+                <h2 className={css(styles.h2)}>
+                  Looking to help out in the wake of the Charlottesville
+                  attacks, but not sure where to send your money?
+                </h2>
+                <div onClick={this.spinWheel} className={css(styles.button)}>
+                  Spin the wheel
+                </div>
+              </div>}
         </div>
       </div>
     );
@@ -69,20 +84,44 @@ const styles = StyleSheet.create({
   wrapper: {
     textAlign: 'center',
     padding: 100,
+    maxWidth: 700,
+    margin: '0 auto',
+  },
+  h1: {
+    marginBottom: 40,
+    fontWeight: 400,
+  },
+  h2: {
+    marginBottom: 40,
+    fontWeight: 400,
+    lineHeight: 1.5,
   },
   button: {
-    background: '#ddd',
-    padding: '10px 20px',
-    borderRadius: 5,
+    background: '#6D8B9C',
+    padding: '30px 40px',
+    borderRadius: 10,
     display: 'inline-block',
-    margin: '20px auto 100px',
     cursor: 'pointer',
     WebkitTouchCallout: 'none',
     WebkitUserSelect: 'none',
     MozUserSelect: 'none',
     userSelect: 'none',
+    textDecoration: 'none',
+    marginBottom: 20,
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 200,
     ':hover': {
-      background: '#ccc',
+      background: '#202C39',
+    },
+  },
+  spinAgain: {
+    textDecoration: 'underline',
+    color: '#000000',
+    cursor: 'pointer',
+    opacity: '0.5',
+    ':hover': {
+      opacity: '0.7',
     },
   },
 });
---
+++ src/recipients.js
@@ -0,0 +1,139 @@
+export const recipients = [
+  {
+    name:
+      'Medical fund for Alexis and Noelle Morris, a Black mother and daughter who were hit by the car',
+    url: 'https://www.youcaring.com/alexismorrisnoellemorris-903027',
+  },
+  {
+    name:
+      'Medical fund for Natalie Romero, an organizer with Movimiento Cosecha who suffered serious injuries',
+    url: 'https://www.gofundme.com/natalie-romero-medical-fund',
+  },
+  {
+    name:
+      'Medical fund for Dre Harris, a 20-year old artist who was assaulted and beaten by white supremacists',
+    url: 'https://www.gofundme.com/i-was-beaten-by-white-supremacists',
+  },
+  {
+    name:
+      'Medical fund for Bill Burke, an ISO member injured in the car attack',
+    url:
+      'https://www.youcaring.com/athensisomemberhurtincharlottesvillecounterprotests-902560',
+  },
+  {
+    name:
+      'Fundraiser to support the family of Heather Heyer, murdered in Charlottesville',
+    url: 'https://www.gofundme.com/our-sisters-keeper-heatherheyer',
+  },
+  {
+    name:
+      'Medical fund organized by Democratic Socialists of America to cover medical expenses and trauma counseling for victims of the attack',
+    url: 'https://www.gofundme.com/medical-fund-for-comrades-in-cville',
+  },
+  {
+    name: 'Black Lives Matter Charlottesville',
+    url: 'https://www.paypal.me/blmcville/',
+  },
+  {
+    name:
+      "Congregate C'ville, a group that organizes and equips faith leaders those who would follow them to show up for justice",
+    url: 'https://restorationvillagearts.org/rva-fellows/congregate-cville/',
+  },
+  {
+    name:
+      'Southerners On New Ground, working towards a vision of liberation for working class, poor, Black, Brown, people of color, women, immigrants, and rural LGBTQ people',
+    url:
+      'https://database.southernersonnewground.org/civicrm/contribute/transact?reset=1&id=2',
+  },
+  {
+    name: 'Albemarle-Charlottesville NAACP',
+    url: 'http://www.albemarle-cvillenaacp.org/',
+  },
+  {
+    name: 'Charlottesville Pride Community Network',
+    url: 'http://cvillepride.org/',
+  },
+  {
+    name: 'Planned Parenthood South Atlantic',
+    url:
+      'https://secure.ppaction.org/site/Donation2?df_id=24415&24415.donation=form1&s_designeeid=1116',
+  },
+  {
+    name: 'Charlottesville National Organization for Women',
+    url: 'http://www.cvillenow.org/involve.html',
+  },
+  {
+    name: 'Charlottesville Meals on Wheels',
+    url: 'https://www.cvillemeals.org/',
+  },
+  {
+    name:
+      'African American Teaching Fellows, working to recruit, support, develop, and retain a cadre of African American teachers to serve the public schools of Charlottesville and Albemarle County.',
+    url: 'http://www.aateachingfellows.org/',
+  },
+  {
+    name:
+      'Impact Charlottesville: Interfaith Movement Promoting Action by Congregations Together',
+    url: 'https://impactcville.com/',
+  },
+  {
+    name:
+      'The Arc of the Piedmont, serving youth and adults in Central Virginia with intellectual and developmental disabilities',
+    url: 'http://arcpva.org/donate/',
+  },
+  {
+    name: 'Big Brothers Big Sisters of the Central Blue Ridge',
+    url: 'http://www.blueridgebigs.org/donate',
+  },
+  {
+    name: 'Brody Jewish Center at the University of Virginia',
+    url: 'https://secure.lglforms.com/form_engine/s/jML0Uah5iSxU5YZKzwBFkg',
+  },
+  {
+    name:
+      'C-ville Victim Relief, a group of community members raising funds to help support the victims of the terrorist attack and Alt-Right demonstration events',
+    url: 'https://www.gofundme.com/united-cville-victim-relief',
+  },
+  {
+    name: 'U.Va Black Student Alliance',
+    url:
+      'https://securelb.imodules.com/s/1535/16-uva/index.aspx?sid=1535&gid=16&pgid=1199&cid=2383&dids=409&BLEDIT=1',
+  },
+  {
+    name: 'International Rescue Committee of Charlottesville',
+    url: 'https://help.rescue.org/donate',
+  },
+  {
+    name:
+      'City of Promise, helping all children in Charlottesville reach their academic and personal potential',
+    url: 'https://www.cityofpromise.org/your-impact',
+  },
+  {
+    name:
+      'Legal Aid Justice Center, providing legal representation for low-income individuals in Virginia',
+    url:
+      'https://salsa4.salsalabs.com/o/51626/p/salsa/donation/common/public/?donate_page_KEY=11637',
+  },
+  {
+    name: 'Solidarity Cville Anti-Racist Legal Fund',
+    url: 'https://fundly.com/solidarity-c-ville-7-8-anti-racist-legal-fund',
+  },
+  {
+    name: 'Showing Up For Racial Justice',
+    url:
+      'https://showingupforracialjustice.nationbuilder.com/one_time_donations',
+  },
+  {
+    name: 'Cville Resists White Supremacy',
+    url: 'https://www.gofundme.com/cville-white-supremacy-resistance',
+  },
+  {
+    name:
+      "The Women's Initiative, providing women in Charlottesville with effective counseling, social support and education so they can transform life challenges into positive change and growth",
+    url: 'http://thewomensinitiative.org/donate/',
+  },
+  {
+    name: 'Islamic Society of Central Virginia',
+    url: 'http://www.charlottesvillemasjid.com/support-us.html',
+  },
+];
