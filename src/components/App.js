d292b4dee58be583c8c7fdc4f8e5657a4efd4589
Emily Maskin <emily.maskin@gmail.com>
2017-08-16 15:10:23 -0400

    new design

--- src/components/App.js
+++ src/components/App.js
@@ -47,8 +47,9 @@ export default class App extends React.Component {
         <div className={css(styles.wrapper)}>
           <h1 className={css(styles.h1)}>
             <div>Donate</div>
-            <div className={css(styles.script)}>Charlottesville</div>
+            <div>Charlottesville</div>
           </h1>
+          <div className={css(styles.divider)} />
           {this.state.recipient
             ? <div>
                 <h2 className={css(styles.h2)}>You should donate to:</h2>
@@ -88,33 +89,54 @@ const styles = StyleSheet.create({
   wrapper: {
     textAlign: 'center',
     padding: 40,
-    maxWidth: 600,
     margin: '0 auto',
+    '@media screen and (max-width: 500px)': {
+      padding: 20,
+    },
+  },
+  divider: {
+    height: 6,
+    width: 400,
+    background: '#fff',
+    margin: '40px auto',
+    maxWidth: '90%',
   },
   h1: {
-    fontWeight: 400,
-    fontSize: 50,
-    lineHeight: 1,
-    marginBottom: 100,
-  },
-  script: {
-    fontFamily: "'Dancing Script', cursive",
     fontSize: 100,
+    lineHeight: 1,
+    fontWeight: 900,
+    fontFamily:
+      "'franklin-gothic-urw', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+    '@media screen and (max-width: 500px)': {
+      fontSize: 44,
+      lineHeight: 1.2,
+      marginBottom: 50,
+    },
+    '@media screen and (max-width: 350px)': {
+      fontSize: 36,
+      lineHeight: 1.2,
+    },
   },
   h2: {
-    fontWeight: 400,
+    fontWeight: 300,
     lineHeight: 1.5,
+    maxWidth: 560,
+    margin: '0 auto 40px',
   },
   h3: {
     marginBottom: 40,
-    fontWeight: 400,
+    fontWeight: 600,
     fontSize: 24,
     lineHeight: 1.3,
+    color: '#3DB0FD',
+    maxWidth: 600,
+    margin: '0 auto 40px',
+    transition: '0.3s ease-out',
   },
   button: {
-    background: '#6D8B9C',
-    padding: '30px 40px',
-    borderRadius: 10,
+    background: '#000',
+    border: '4px solid #fff',
+    padding: '25px 50px',
     display: 'inline-block',
     cursor: 'pointer',
     WebkitTouchCallout: 'none',
@@ -122,21 +144,30 @@ const styles = StyleSheet.create({
     MozUserSelect: 'none',
     userSelect: 'none',
     textDecoration: 'none',
-    margin: 40,
-    color: '#fff',
+    marginBottom: 40,
     fontSize: 30,
-    fontWeight: 200,
+    fontWeight: 500,
+    textTransform: 'uppercase',
+    transition: '0.3s ease-out',
     ':hover': {
-      background: '#202C39',
+      color: '#000',
+      background: '#fff',
+      transition: '0.2s ease-out',
+    },
+    '@media screen and (max-width: 500px)': {
+      fontSize: 20,
+      lineHeight: 1.2,
+      padding: '12px 24px',
+      margin: '20px auto',
     },
   },
   spinAgain: {
-    textDecoration: 'underline',
-    color: '#000000',
+    color: '#aaa',
     cursor: 'pointer',
-    opacity: '0.5',
+    fontSize: 24,
+    fontWeight: 300,
     ':hover': {
-      opacity: '0.7',
+      color: '#ccc',
     },
   },
 });
